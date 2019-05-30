38209eca9fa00957843030578f8b8f53df159992
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-05-30 19:56:26 +0530

    Json with array
    Controller module

---
+++ custom2.js
@@ -0,0 +1,15 @@
+var myapp=angular.module("ar",[]);
+myapp.controller("mycontroller",  function($scope){
+	$scope.city["kolkata","bhubonewar","hydrabad","durgapur"];
+	
+	$scope.details[
+    {"name":"arnok","city":"kolkata"},
+    {"name":"tutorial point","city":"durgapur"},
+    {"name":"tony","city":"bhubonewar"},
+    {"name":"stark","city":"hydrabad"}
+
+	];
+	
+});
+
+
