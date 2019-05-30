641f72f79755df2222e2a95ec756749582043c13
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-05-30 14:27:27 +0530

    Add files via upload

--- custom.js.js
+++ custom.js.js
@@ -1,8 +1,11 @@
 
 var mymodule=angular.module("js",[]);
 mymodule.controller("mycontroller",  function($scope){
-	$scope.firstname="koushik";
-	$scope.lastname="bose";
-alert("hello es6............!!");	
+	$scope.name="koushik";
+	$scope.city="kolkata";
+	$scope.detail=function(){
+		return "this is function variable: "+$scope.name+$scope.city;
+	};
+	
 });
 
