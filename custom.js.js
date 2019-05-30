a5c0793f224e12f51b1b4b44b3fd2ab1bc354f41
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-05-30 12:08:48 +0530

    module creation in angular.js

---
+++ custom.js.js
@@ -0,0 +1,8 @@
+
+var mymodule=angular.module("js",[]);
+mymodule.controller("mycontroller",  function($scope){
+	$scope.firstname="koushik";
+	$scope.lastname="bose";
+alert("hello es6............!!");	
+});
+
